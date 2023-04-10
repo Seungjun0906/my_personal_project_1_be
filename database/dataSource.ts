91db5fae0f9741f1b3a778109705447f19258613
Jun <87404676+Seungjun0906@users.noreply.github.com>
2023-04-10 22:03:35 +0900

    기본세팅

---
+++ database/dataSource.ts
@@ -0,0 +1,16 @@
+import { DataSource } from 'typeorm';
+import * as dotenv from 'dotenv';
+
+dotenv.config({
+  path: __dirname + '/../configs//env/.development.env',
+});
+
+export const AppDataSource = new DataSource({
+  type: 'mysql',
+  host: process.env.DB_HOST,
+  port: 3306,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  migrations: ['migrations/*{.ts,.js'],
+});
