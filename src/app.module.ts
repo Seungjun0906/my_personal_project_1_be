eed8a258d8737e3de6a01bd7c934758bf14fdfd7
Jun <87404676+Seungjun0906@users.noreply.github.com>
2023-04-16 23:40:36 +0900

    git ignore 수정

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,20 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ScheduleModule } from '@nestjs/schedule';
+import { CatsController } from './cats/cats.controller';
+import { Telegram } from './telegram/telegram';
+import { TelegramModuleModule } from './telegram.module/telegram.module.module';
+import { TelegramModule } from './telegram/telegram.module';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ScheduleModule.forRoot(),
+    ConfigModule.forRoot(),
+    TelegramModuleModule,
+    TelegramModule,
+  ],
+  controllers: [AppController, CatsController],
+  providers: [AppService, Telegram],
 })
 export class AppModule {}
